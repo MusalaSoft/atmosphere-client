a21e2fe32b5a6ce5f8da54dbadb4c86f76acd9b5
Dimcho N. Nedev <dimcho.nedev@musala.com>
2017-05-19 18:24:40 +0300

    Update Client's tests

--- src/test/java/com/musala/atmosphere/client/InstallApkTest.java
+++ src/test/java/com/musala/atmosphere/client/InstallApkTest.java
@@ -12,24 +12,24 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.lang.reflect.Field;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
-import com.musala.atmosphere.commons.DeviceInformation;
+import com.musala.atmosphere.client.websocket.ClientDispatcher;
 import com.musala.atmosphere.commons.RoutingAction;
-import com.musala.atmosphere.commons.cs.clientdevice.IClientDevice;
 import com.musala.atmosphere.commons.exceptions.CommandFailedException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class InstallApkTest {
     private static final int TEST_PASSKEY = 0;
 
-    private IClientDevice innerClientDeviceMock;
+    private static final String TEST_DEVICE_ID = "test_device_id";
+
+    private ClientDispatcher dispatcherMock;
 
     private DeviceCommunicator deviceCommunicator;
 
@@ -37,68 +37,59 @@ public class InstallApkTest {
 
     @Before
     public void setUpDevice() throws Exception {
-        DeviceInformation deviceInfoMock = mock(DeviceInformation.class);
-        innerClientDeviceMock = mock(IClientDevice.class);
-        when(innerClientDeviceMock.route(eq(TEST_PASSKEY),
-                                         eq(RoutingAction.GET_DEVICE_INFORMATION))).thenReturn(deviceInfoMock);
-        deviceCommunicator = new DeviceCommunicator(innerClientDeviceMock, TEST_PASSKEY);
-
+        dispatcherMock = mock(ClientDispatcher.class);
+        deviceCommunicator = new DeviceCommunicator(TEST_PASSKEY, TEST_DEVICE_ID);
+        
+        Class<?> deviceCommunicatorClass = deviceCommunicator.getClass();
+        Field dispatcher = deviceCommunicatorClass.getDeclaredField("dispatcher");
+        dispatcher.setAccessible(true);
+        dispatcher.set(deviceCommunicator, dispatcherMock);
+       
         device = new Device(deviceCommunicator);
     }
 
     @Test
-    public void apkFileNotFoundTest() {
+    public void apkFileNotFoundTest() throws Exception {
+        DeviceCommunicator deviceCommunicatorMock = mock(DeviceCommunicator.class);
+        when(deviceCommunicatorMock.sendAction(RoutingAction.APK_INIT_INSTALL)).thenReturn(DeviceCommunicator.VOID_SUCCESS);
+
+        Device device = new Device(deviceCommunicatorMock);
+
         assertFalse(device.installAPK(TestResources.PATH_TO_NOT_EXISTING_APK_FILE));
     }
 
     @Test
     public void apkFileInitializationErrorTest() throws Exception {
-        doThrow(new CommandFailedException()).when(innerClientDeviceMock).route(anyLong(),
-                                                                                eq(RoutingAction.APK_INIT_INSTALL));
+        doThrow(new CommandFailedException()).when(dispatcherMock).route(any(),
+                                                                         anyLong(),
+                                                                         eq(RoutingAction.APK_INIT_INSTALL));
+
         assertFalse(device.installAPK(TestResources.PATH_TO_APK_FILE));
-        verify(innerClientDeviceMock, times(1)).route(anyLong(), eq(RoutingAction.APK_INIT_INSTALL));
+        verify(dispatcherMock, times(1)).route(any(), anyLong(), eq(RoutingAction.APK_INIT_INSTALL));
     }
 
     @Test
     public void appendingErrorTest() throws Exception {
-        Answer<Object> routeAnswer = new Answer<Object>() {
-
-            @Override
-            public Object answer(InvocationOnMock invocation) throws Throwable {
-                Object[] args = invocation.getArguments();
-                RoutingAction action = (RoutingAction) args[1];
-                switch (action) {
-                    case APK_INIT_INSTALL:
-                        return null;
-                    case APK_APPEND_DATA:
-                        throw new CommandFailedException();
-                    default:
-                        break;
-                }
-                return null;
-            }
-        };
-        Mockito.doAnswer(routeAnswer).when(innerClientDeviceMock).route(anyLong(), any(RoutingAction.class));
-        Mockito.doAnswer(routeAnswer).when(innerClientDeviceMock).route(anyLong(),
-                                                                        any(RoutingAction.class),
-                                                                        any(),
-                                                                        anyInt());
+        when(dispatcherMock.route(any(), anyLong(), eq(RoutingAction.APK_INIT_INSTALL))).thenReturn(null);
+        doThrow(new CommandFailedException()).when(dispatcherMock)
+                                             .route(any(),
+                                                    anyLong(),
+                                                    eq(RoutingAction.APK_APPEND_DATA),
+                                                    any(),
+                                                    anyInt());
 
         assertFalse(device.installAPK(TestResources.PATH_TO_APK_FILE));
-        verify(innerClientDeviceMock, times(1)).route(anyLong(), eq(RoutingAction.APK_APPEND_DATA), any(), anyLong());
+        verify(dispatcherMock, times(1)).route(any(), anyLong(), eq(RoutingAction.APK_APPEND_DATA), any(), anyLong());
     }
 
     @Test
     public void installationFailedCommandExecutionTest() throws Exception {
-        doThrow(new CommandFailedException()).when(innerClientDeviceMock)
-                                             .route(anyLong(), eq(RoutingAction.APK_BUILD_AND_INSTALL), anyBoolean());
-        assertFalse(device.installAPK(TestResources.PATH_TO_APK_FILE));
-    }
+        doThrow(new CommandFailedException()).when(dispatcherMock)
+                                             .route(any(),
+                                                    anyLong(),
+                                                    eq(RoutingAction.APK_BUILD_AND_INSTALL),
+                                                    anyBoolean());
 
-    @Test
-    public void installationWritingOnWrappedDeviceErrorTest() throws Exception {
-        doThrow(new CommandFailedException()).when(innerClientDeviceMock)
-                                             .route(anyLong(), eq(RoutingAction.APK_BUILD_AND_INSTALL), anyBoolean());
         assertFalse(device.installAPK(TestResources.PATH_TO_APK_FILE));
     }
 
